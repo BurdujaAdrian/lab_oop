48c55939dc989c690f85fb9104f22a6ed0d221fa
BurdujaAdrian <adrianburduja7.3@gmail.com>
2024-11-25 21:34:22 +0200

    finished task2, created the structs, traits and their implementation. Also written tested for proper behaviour of shared mutability

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 #![allow(unused)]
 
+use std::{
+    borrow::BorrowMut,
+    cell::{RefCell, RefMut},
+};
 fn main() {
     println!("Hello, world!");
+    todo!("Write tests for new traits")
 }
 
 //
@@ -17,80 +22,73 @@ pub trait Queue {
 }
 
 pub trait Dineable {
-    fn serve_dinner(car_id: String);
+    fn serve_dinner(&mut self, car_id: String) {
+        println!("Serving for car: {car_id}")
+    }
 }
 
 pub trait Refuelable {
-    fn refuel(car_id: String);
+    fn refuel(&mut self, car_id: String) {
+        println!("Refuiling car: {car_id}")
+    }
 }
 
 //
 // @structs
 //
 
-struct PeopleDinner {}
-struct RobotDinner {}
-struct ElectricStation {}
-struct GasStation {}
+struct PeopleDinner {
+    dined: *mut i32,
+}
+struct RobotDinner {
+    dined: *mut i32,
+}
+struct ElectricStation {
+    refueled: *mut i32,
+}
+struct GasStation {
+    refueled: *mut i32,
+}
 
 //
 // @implementations
 //
 
-/*
-impl Queue for Vec<i32> {
-    type Item = i32;
-
-    fn enque(&mut self, item: Self::Item) {
-        self.push(item);
-    }
-
-    fn deque(&mut self) -> Option<Self::Item> {
-        if self.len() == 0 {
-            return None;
+impl Dineable for PeopleDinner {
+    fn serve_dinner(&mut self, car_id: String) {
+        unsafe {
+            *self.dined += 1;
         }
-        let res = self[0].clone();
-        self.remove(0);
-
-        Some(res)
-    }
-
-    fn rear(&self) -> Option<&Self::Item> {
-        return self.last();
-    }
-
-    fn front(&self) -> Option<&Self::Item> {
-        return self.first();
+        println!("Serving people in car: {car_id}")
     }
 }
-*/
 
-/*
-impl Queue for Vec<String> {
-    type Item = String;
-
-    fn enque(&mut self, item: Self::Item) {
-        self.push(item);
-    }
-    fn deque(&mut self) -> Option<Self::Item> {
-        if self.len() == 0 {
-            return None;
+impl Dineable for RobotDinner {
+    fn serve_dinner(&mut self, car_id: String) {
+        unsafe {
+            *self.dined += 1;
         }
-        let res = self[0].clone();
-        self.remove(0);
-
-        Some(res)
-    }
-
-    fn rear(&self) -> Option<&Self::Item> {
-        return self.last();
-    }
-
-    fn front(&self) -> Option<&Self::Item> {
-        return self.first();
+        println!("Serving robots in car: {car_id}")
     }
 }
-*/
+
+impl Refuelable for ElectricStation {
+    fn refuel(&mut self, car_id: String) {
+        unsafe {
+            *self.refueled += 1;
+        }
+        println!("Recharging electric car: {car_id}")
+    }
+}
+
+impl Refuelable for GasStation {
+    fn refuel(&mut self, car_id: String) {
+        unsafe {
+            *self.refueled += 1;
+        }
+        println!("Refueling gas car: {car_id}")
+    }
+}
 
 impl<T: Clone> Queue for Vec<T> {
     type Item = T;
@@ -215,3 +213,72 @@ fn test_queue_struct() {
     assert_eq!(queue.rear(), None);
     assert_eq!(queue.front(), None);
 }
+
+#[test]
+fn test_dining() {
+    let mut n_of_people_who_dined: i32 = 0;
+    let mut people_diner1 = PeopleDinner {
+        dined: &mut n_of_people_who_dined,
+    };
+    let mut people_diner2 = PeopleDinner {
+        dined: &mut n_of_people_who_dined,
+    };
+
+    people_diner2.serve_dinner("1".to_string());
+    people_diner1.serve_dinner("2".to_string());
+
+    assert_eq!(n_of_people_who_dined, 2);
+
+    let mut n_of_robots_who_dined: i32 = 0;
+    let mut robot_diner1 = RobotDinner {
+        dined: &mut n_of_robots_who_dined,
+    };
+
+    let mut robot_diner2 = RobotDinner {
+        dined: &mut n_of_robots_who_dined,
+    };
+    let mut robot_diner3 = RobotDinner {
+        dined: &mut n_of_robots_who_dined,
+    };
+
+    robot_diner1.serve_dinner("3".to_string());
+    robot_diner3.serve_dinner("1".to_string());
+    robot_diner2.serve_dinner("2".to_string());
+
+    assert_eq!(n_of_robots_who_dined, 3);
+}
+
+#[test]
+fn test_refueling() {
+    let mut n_of_electric_cars_refueled: i32 = 0;
+    let mut electric_station1 = ElectricStation {
+        refueled: &mut n_of_electric_cars_refueled,
+    };
+    let mut electric_station2 = ElectricStation {
+        refueled: &mut n_of_electric_cars_refueled,
+    };
+
+    electric_station1.refuel("ElectricCar1".to_string());
+    electric_station2.refuel("ElectricCar2".to_string());
+
+    assert_eq!(n_of_electric_cars_refueled, 2);
+
+    let mut n_of_gas_cars_refueled: i32 = 0;
+    let mut gas_station1 = GasStation {
+        refueled: &mut n_of_gas_cars_refueled,
+    };
+    let mut gas_station2 = GasStation {
+        refueled: &mut n_of_gas_cars_refueled,
+    };
+    let mut gas_station3 = GasStation {
+        refueled: &mut n_of_gas_cars_refueled,
+    };
+
+    gas_station1.refuel("GasCar1".to_string());
+    gas_station3.refuel("GasCar3".to_string());
+    gas_station2.refuel("GasCar2".to_string());
+
+    assert_eq!(n_of_gas_cars_refueled, 3);
+}
+
+//@end of tests
