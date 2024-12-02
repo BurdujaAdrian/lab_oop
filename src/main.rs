90b7576733f40c0c58e0bd0e7396ac4ad082c6c5
BurdujaAdrian <adrianburduja7.3@gmail.com>
2024-12-02 12:53:38 +0200

    finished task 3

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
-#![allow(unused)]
+#![allow(unused, non_snake_case)]
 
+use serde::{Deserialize, Serialize};
+use serde_json;
 use std::{
     borrow::BorrowMut,
     cell::{RefCell, RefMut},
+    fs, io,
 };
+
 fn main() {
     println!("Hello, world!");
-    todo!("Write tests for new traits")
+    test_semaphore_work();
 }
 
 //
@@ -21,72 +25,257 @@ pub trait Queue {
     fn front(&self) -> Option<&Self::Item>;
 }
 
-pub trait Dineable {
-    fn serve_dinner(&mut self, car_id: String) {
-        println!("Serving for car: {car_id}")
+trait Dineable {
+    fn serve_dinner(&mut self, car: Car) {
+        println!("Serving for car: {:?}", car.id)
     }
 }
 
-pub trait Refuelable {
-    fn refuel(&mut self, car_id: String) {
-        println!("Refuiling car: {car_id}")
+trait Refuelable {
+    fn refuel(&mut self, car: Car) {
+        println!("Refuiling car: {:?}", car.id)
     }
 }
 
+//
+// @enums
+//
+
+#[derive(Clone, Copy, Debug, Serialize, Deserialize, PartialEq)]
+enum CarType {
+    ELECTRIC,
+    GAS,
+}
+
+#[derive(Clone, Copy, Debug, Serialize, Deserialize, PartialEq)]
+enum PassangerType {
+    PEOPLE,
+    ROBOTS,
+}
 //
 // @structs
 //
 
+struct Semaphore {
+    GasStation_forHumans: CarStation,
+    GasStation_forRobots: CarStation,
+    ElectroStation_forHumans: CarStation,
+    ElectroStation_forRobots: CarStation,
+}
+
 struct PeopleDinner {
     dined: *mut i32,
+    did_not: *mut i32,
 }
 struct RobotDinner {
     dined: *mut i32,
+    did_not: *mut i32,
 }
 struct ElectricStation {
     refueled: *mut i32,
+    consumption: *mut u32,
 }
 struct GasStation {
     refueled: *mut i32,
+    consumption: *mut u32,
+}
+
+#[derive(Clone, Copy, Debug, Serialize, Deserialize)]
+struct Car {
+    id: u32,
+    r#type: CarType,
+    passengers: PassangerType,
+    isDining: bool,
+    consumption: u32,
+}
+
+struct CarStation {
+    dining_service: Box<dyn Dineable>,
+    refueling_service: Box<dyn Refuelable>,
+    queue: Vec<Car>,
 }
 
 //
 // @implementations
 //
 
-impl Dineable for PeopleDinner {
-    fn serve_dinner(&mut self, car_id: String) {
-        unsafe {
-            *self.dined += 1;
+// n_who_did_not: i32 = 0;
+// n_of_people_who_dined: i32 = 0;
+// n_of_robots_who_dined: i32 = 0;
+// n_of_electric_cars_refueled: i32 = 0;
+// n_of_gas_cars_refueled: i32 = 0;
+// electric_cons: u32 = 0;
+// gass_cons: u32 = 0;
+
+impl Semaphore {
+    fn init(
+        n_didnt: &mut i32,
+        n_r_dined: &mut i32,
+        n_p_dined: &mut i32,
+        n_e_refueled: &mut i32,
+        n_g_refueled: &mut i32,
+        el_cons: &mut u32,
+        gas_cons: &mut u32,
+    ) -> Self {
+        Self {
+            GasStation_forHumans: CarStation {
+                dining_service: Box::new(PeopleDinner {
+                    dined: n_p_dined,
+                    did_not: n_didnt,
+                }),
+                refueling_service: Box::new(GasStation {
+                    refueled: n_g_refueled,
+                    consumption: gas_cons,
+                }),
+                queue: vec![],
+            },
+
+            GasStation_forRobots: CarStation {
+                dining_service: Box::new(RobotDinner {
+                    dined: n_r_dined,
+                    did_not: n_didnt,
+                }),
+                refueling_service: Box::new(GasStation {
+                    refueled: n_g_refueled,
+                    consumption: gas_cons,
+                }),
+                queue: vec![],
+            },
+
+            ElectroStation_forHumans: CarStation {
+                dining_service: Box::new(PeopleDinner {
+                    dined: n_p_dined,
+                    did_not: n_didnt,
+                }),
+                refueling_service: Box::new(ElectricStation {
+                    refueled: n_e_refueled,
+                    consumption: el_cons,
+                }),
+                queue: vec![],
+            },
+            ElectroStation_forRobots: CarStation {
+                dining_service: Box::new(RobotDinner {
+                    dined: n_r_dined,
+                    did_not: n_didnt,
+                }),
+                refueling_service: Box::new(ElectricStation {
+                    refueled: n_e_refueled,
+                    consumption: el_cons,
+                }),
+                queue: vec![],
+            },
         }
-        println!("Serving people in car: {car_id}")
+    }
+    fn work(&mut self) -> io::Result<()> {
+        let path = "./queue";
+
+        let entries = fs::read_dir(path)?;
+
+        let file_names: Vec<String> = entries
+            .filter_map(|entry| {
+                let path = entry.ok()?.path();
+                if path.is_file() {
+                    path.file_name()?.to_str().map(|s| s.to_owned())
+                } else {
+                    None
+                }
+            })
+            .collect();
+
+        let cars: Vec<Car> = file_names
+            .into_iter()
+            .map(|path| {
+                println!("path:{}", "./queue/".to_owned() + &path);
+                serde_json::from_str(&fs::read_to_string("./queue/".to_owned() + &path).unwrap())
+                    .expect("failed to parse json")
+            })
+            .collect();
+
+        cars.iter().for_each(|car| {
+            if car.passengers == PassangerType::PEOPLE {
+                if car.r#type == CarType::GAS {
+                    self.GasStation_forHumans.add_car(*car);
+                    return;
+                }
+                // else its electric
+                self.ElectroStation_forHumans.add_car(*car);
+                return;
+            }
+            // else its for robots
+            if car.r#type == CarType::GAS {
+                self.GasStation_forRobots.add_car(*car);
+                return;
+            }
+            // else its electric
+            self.ElectroStation_forRobots.add_car(*car);
+            return;
+        });
+        println!("{:#?}", cars);
+        Ok(())
+    }
+}
+
+impl CarStation {
+    fn serve_cars(&mut self) {
+        while let Some(car) = self.queue.deque() {
+            self.dining_service.serve_dinner(car);
+            self.refueling_service.refuel(car);
+        }
+    }
+
+    fn add_car(&mut self, car: Car) {
+        self.queue.enque(car);
+    }
+}
+
+impl Dineable for PeopleDinner {
+    fn serve_dinner(&mut self, car: Car) {
+        unsafe {
+            // for simple and dirty shared mutable data
+            if car.isDining {
+                *self.dined += 1;
+            } else {
+                *self.did_not += 1;
+            }
+        }
+        println!("Serving people in car: {:?}", car.id)
     }
 }
 
 impl Dineable for RobotDinner {
-    fn serve_dinner(&mut self, car_id: String) {
+    fn serve_dinner(&mut self, car: Car) {
         unsafe {
-            *self.dined += 1;
+            if car.isDining {
+                *self.dined += 1;
+            } else {
+                *self.did_not += 1;
+            }
         }
-        println!("Serving robots in car: {car_id}")
+        println!("Serving robots in car: {:?}", car.id)
     }
 }
 
 impl Refuelable for ElectricStation {
-    fn refuel(&mut self, car_id: String) {
+    fn refuel(&mut self, car: Car) {
         unsafe {
-            *self.refueled += 1;
+            if car.consumption > 0 {
+                *self.refueled += 1;
+                *self.consumption += car.consumption;
+            }
         }
-        println!("Recharging electric car: {car_id}")
+        println!("Recharging electric car: {:?}", car.id)
     }
 }
 
 impl Refuelable for GasStation {
-    fn refuel(&mut self, car_id: String) {
+    fn refuel(&mut self, car: Car) {
         unsafe {
-            *self.refueled += 1;
+            if car.consumption > 0 {
+                *self.refueled += 1;
+                *self.consumption += car.consumption;
+            }
         }
-        println!("Refueling gas car: {car_id}")
+        println!("Refueling gas car: {:?}", car.id)
     }
 }
 
@@ -217,68 +406,299 @@ fn test_queue_struct() {
 #[test]
 fn test_dining() {
     let mut n_of_people_who_dined: i32 = 0;
+    let mut n_who_did_not: i32 = 0;
     let mut people_diner1 = PeopleDinner {
         dined: &mut n_of_people_who_dined,
+        did_not: &mut n_who_did_not,
     };
     let mut people_diner2 = PeopleDinner {
         dined: &mut n_of_people_who_dined,
+        did_not: &mut n_who_did_not,
     };
 
-    people_diner2.serve_dinner("1".to_string());
-    people_diner1.serve_dinner("2".to_string());
+    let car1 = Car {
+        id: 1,
+        r#type: CarType::GAS,
+        passengers: PassangerType::PEOPLE,
+        isDining: true,
+        consumption: 0,
+    };
+    let car2 = Car {
+        id: 2,
+        r#type: CarType::ELECTRIC,
+        passengers: PassangerType::PEOPLE,
+        isDining: false,
+        consumption: 0,
+    };
 
-    assert_eq!(n_of_people_who_dined, 2);
+    people_diner1.serve_dinner(car1);
+    people_diner2.serve_dinner(car2);
+
+    assert_eq!(n_of_people_who_dined, 1);
 
     let mut n_of_robots_who_dined: i32 = 0;
     let mut robot_diner1 = RobotDinner {
         dined: &mut n_of_robots_who_dined,
+        did_not: &mut n_who_did_not,
     };
-
     let mut robot_diner2 = RobotDinner {
         dined: &mut n_of_robots_who_dined,
-    };
-    let mut robot_diner3 = RobotDinner {
-        dined: &mut n_of_robots_who_dined,
+        did_not: &mut n_who_did_not,
     };
 
-    robot_diner1.serve_dinner("3".to_string());
-    robot_diner3.serve_dinner("1".to_string());
-    robot_diner2.serve_dinner("2".to_string());
+    let car3 = Car {
+        id: 3,
+        r#type: CarType::GAS,
+        passengers: PassangerType::ROBOTS,
+        isDining: false,
+        consumption: 0,
+    };
 
-    assert_eq!(n_of_robots_who_dined, 3);
+    let car4 = Car {
+        id: 4,
+        r#type: CarType::GAS,
+        passengers: PassangerType::ROBOTS,
+        isDining: true,
+        consumption: 0,
+    };
+
+    robot_diner1.serve_dinner(car3);
+    robot_diner2.serve_dinner(car4);
+
+    assert_eq!(n_of_robots_who_dined, 1);
+
+    assert_eq!(n_who_did_not, 2);
 }
 
 #[test]
 fn test_refueling() {
     let mut n_of_electric_cars_refueled: i32 = 0;
+    let mut electric_cons: u32 = 0;
     let mut electric_station1 = ElectricStation {
         refueled: &mut n_of_electric_cars_refueled,
+        consumption: &mut electric_cons,
     };
     let mut electric_station2 = ElectricStation {
         refueled: &mut n_of_electric_cars_refueled,
+        consumption: &mut electric_cons,
     };
 
-    electric_station1.refuel("ElectricCar1".to_string());
-    electric_station2.refuel("ElectricCar2".to_string());
+    let car1 = Car {
+        id: 1,
+        r#type: CarType::ELECTRIC,
+        passengers: PassangerType::PEOPLE,
+        isDining: false,
+        consumption: 0,
+    };
 
-    assert_eq!(n_of_electric_cars_refueled, 2);
+    let car2 = Car {
+        id: 2,
+        r#type: CarType::ELECTRIC,
+        passengers: PassangerType::ROBOTS,
+        isDining: false,
+        consumption: 10,
+    };
+
+    electric_station1.refuel(car1);
+    electric_station2.refuel(car2);
+
+    assert_eq!(n_of_electric_cars_refueled, 1);
 
     let mut n_of_gas_cars_refueled: i32 = 0;
+    let mut gass_cons: u32 = 0;
     let mut gas_station1 = GasStation {
         refueled: &mut n_of_gas_cars_refueled,
+        consumption: &mut gass_cons,
     };
+
+    let car3 = Car {
+        id: 3,
+        r#type: CarType::GAS,
+        passengers: PassangerType::PEOPLE,
+        isDining: false,
+        consumption: 0,
+    };
+
+    gas_station1.refuel(car3);
+
+    assert_eq!(n_of_gas_cars_refueled, 0);
+
     let mut gas_station2 = GasStation {
         refueled: &mut n_of_gas_cars_refueled,
-    };
-    let mut gas_station3 = GasStation {
-        refueled: &mut n_of_gas_cars_refueled,
+        consumption: &mut gass_cons,
     };
 
-    gas_station1.refuel("GasCar1".to_string());
-    gas_station3.refuel("GasCar3".to_string());
-    gas_station2.refuel("GasCar2".to_string());
+    let car4 = Car {
+        id: 4,
+        r#type: CarType::GAS,
+        passengers: PassangerType::PEOPLE,
+        isDining: false,
+        consumption: 20,
+    };
 
+    gas_station2.refuel(car4);
+
+    assert_eq!(n_of_gas_cars_refueled, 1);
+
+    assert_eq!(gass_cons, 20);
+    assert_eq!(electric_cons, 10);
+}
+
+#[test]
+fn test_serving() {
+    let mut n_who_did_not: i32 = 0;
+    let mut n_of_people_who_dined: i32 = 0;
+    let mut n_of_robots_who_dined: i32 = 0;
+    let mut n_of_electric_cars_refueled: i32 = 0;
+    let mut n_of_gas_cars_refueled: i32 = 0;
+    let mut electric_cons: u32 = 0;
+    let mut gass_cons: u32 = 0;
+
+    let car_queue = vec![
+        Car {
+            id: 1,
+            r#type: CarType::ELECTRIC,
+            passengers: PassangerType::PEOPLE,
+            isDining: true,
+            consumption: 10,
+        },
+        Car {
+            id: 2,
+            r#type: CarType::GAS,
+            passengers: PassangerType::PEOPLE,
+            isDining: true,
+            consumption: 10,
+        },
+        Car {
+            id: 3,
+            r#type: CarType::ELECTRIC,
+            passengers: PassangerType::ROBOTS,
+            isDining: true,
+            consumption: 10,
+        },
+        Car {
+            id: 4,
+            r#type: CarType::GAS,
+            passengers: PassangerType::ROBOTS,
+            isDining: true,
+            consumption: 10,
+        },
+        Car {
+            id: 5,
+            r#type: CarType::GAS,
+            passengers: PassangerType::PEOPLE,
+            isDining: false,
+            consumption: 10,
+        },
+        Car {
+            id: 6,
+            r#type: CarType::GAS,
+            passengers: PassangerType::ROBOTS,
+            isDining: false,
+            consumption: 0,
+        },
+        Car {
+            id: 7,
+            r#type: CarType::ELECTRIC,
+            passengers: PassangerType::PEOPLE,
+            isDining: false,
+            consumption: 0,
+        },
+        Car {
+            id: 8,
+            r#type: CarType::ELECTRIC,
+            passengers: PassangerType::ROBOTS,
+            isDining: true,
+            consumption: 0,
+        },
+    ];
+
+    let mut robo_electric_station = CarStation {
+        dining_service: Box::new(RobotDinner {
+            dined: &mut n_of_robots_who_dined,
+            did_not: &mut n_who_did_not,
+        }),
+        refueling_service: Box::new(ElectricStation {
+            refueled: &mut n_of_electric_cars_refueled,
+            consumption: &mut electric_cons,
+        }),
+        queue: vec![car_queue[2], car_queue[7]],
+    };
+    let mut robo_gas_station = CarStation {
+        dining_service: Box::new(RobotDinner {
+            dined: &mut n_of_robots_who_dined,
+            did_not: &mut n_who_did_not,
+        }),
+        refueling_service: Box::new(GasStation {
+            refueled: &mut n_of_gas_cars_refueled,
+            consumption: &mut gass_cons,
+        }),
+        queue: vec![car_queue[3], car_queue[5]],
+    };
+
+    let mut human_gas_station = CarStation {
+        dining_service: Box::new(PeopleDinner {
+            dined: &mut n_of_people_who_dined,
+            did_not: &mut n_who_did_not,
+        }),
+        refueling_service: Box::new(GasStation {
+            refueled: &mut n_of_gas_cars_refueled,
+            consumption: &mut gass_cons,
+        }),
+        queue: vec![car_queue[1], car_queue[4]],
+    };
+
+    let mut human_electric_station = CarStation {
+        dining_service: Box::new(PeopleDinner {
+            dined: &mut n_of_people_who_dined,
+            did_not: &mut n_who_did_not,
+        }),
+        refueling_service: Box::new(ElectricStation {
+            refueled: &mut n_of_electric_cars_refueled,
+            consumption: &mut electric_cons,
+        }),
+        queue: vec![car_queue[0], car_queue[6]],
+    };
+
+    robo_electric_station.serve_cars();
+
+    human_gas_station.serve_cars();
+
+    robo_gas_station.serve_cars();
+
+    human_electric_station.serve_cars();
+
+    assert_eq!(n_who_did_not, 3);
+    assert_eq!(n_of_people_who_dined, 2);
+    assert_eq!(n_of_robots_who_dined, 3);
+    assert_eq!(n_of_electric_cars_refueled, 2);
     assert_eq!(n_of_gas_cars_refueled, 3);
+    assert_eq!(electric_cons, 20);
+    assert_eq!(gass_cons, 30);
+}
+
+//#[test]
+fn test_semaphore_work() {
+    let mut n_who_did_not: i32 = 0;
+    let mut n_of_people_who_dined: i32 = 0;
+    let mut n_of_robots_who_dined: i32 = 0;
+    let mut n_of_electric_cars_refueled: i32 = 0;
+    let mut n_of_gas_cars_refueled: i32 = 0;
+    let mut electric_cons: u32 = 0;
+    let mut gas_cons: u32 = 0;
+
+    let mut semaphore = Semaphore::init(
+        &mut n_who_did_not,
+        &mut n_of_robots_who_dined,
+        &mut n_of_people_who_dined,
+        &mut n_of_electric_cars_refueled,
+        &mut n_of_gas_cars_refueled,
+        &mut electric_cons,
+        &mut gas_cons,
+    );
+
+    semaphore.work();
+    //    panic!("end the test");
 }
 
 //@end of tests
